1cb8535cb48d51dd2aeec50021a2f831b60d1c59
RokyzasLt <130046690+RokyzasLt@users.noreply.github.com>
2024-05-19 19:33:22 +0300

    Add files via upload

---
+++ Core/Inc/Statechart.d
@@ -0,0 +1,5 @@
+Core/Inc/Statechart.o: ../Core/Inc/Statechart.c ../Core/Inc/sc_types.h \
+ ../Core/Inc/Statechart.h ../Core/Inc/Statechart_required.h
+../Core/Inc/sc_types.h:
+../Core/Inc/Statechart.h:
+../Core/Inc/Statechart_required.h:

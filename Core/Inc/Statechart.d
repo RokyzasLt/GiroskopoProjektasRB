4db91401ae2a8dc233d80927826f3830db25d432
RokyzasLt <130046690+RokyzasLt@users.noreply.github.com>
2024-05-19 19:36:31 +0300

    Delete Core directory

--- Core/Inc/Statechart.d
+++
@@ -1,5 +0,0 @@
-Core/Inc/Statechart.o: ../Core/Inc/Statechart.c ../Core/Inc/sc_types.h \
- ../Core/Inc/Statechart.h ../Core/Inc/Statechart_required.h
-../Core/Inc/sc_types.h:
-../Core/Inc/Statechart.h:
-../Core/Inc/Statechart_required.h:
